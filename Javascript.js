17e08ca07b930b124c26cd84fd8804223736c117
PK-5 <karumalil.p@maristcollege.school.nz>
2021-03-09 12:54:20 +1300

    button

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+
 <button type="button" 
 onclick='document.getElementById("demo").innerHTML = date() > Click here to view date and time
 </button>
-=======
-<button type="button" onclick='document.getElementById("Change").innerHTML = "Thank you for your feedback"'>Submit</button>
->>>>>>> 8fcaf374b1cfa55a58c057d679d068009ee109de
+
